3da19c8708b4cbae4d3b433c4dc14f2f4e5ce09e
mehtavandit <65803900+mehtavandit@users.noreply.github.com>
2022-01-15 15:00:12 +0530

    Create day_2_exercise_3.py

---
+++ day 2/day_2_exercise_3.py
@@ -0,0 +1,24 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+
+
+age_rem = 90-int(age)
+
+mon_rem = age_rem*12
+
+week_rem = age_rem*52
+
+days_rem = age_rem*365
+
+print(f"You have {days_rem} days, {week_rem} weeks, and {mon_rem} months left")
+
+
+
+
+
+
+
