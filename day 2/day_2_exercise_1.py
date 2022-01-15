6ee8d526782e980bb5c616a97ee32fffe028b42e
mehtavandit <65803900+mehtavandit@users.noreply.github.com>
2022-01-15 14:58:51 +0530

    Create day_2_exercise_1.py

---
+++ day 2/day_2_exercise_1.py
@@ -0,0 +1,22 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+a = str(two_digit_number)
+first_str = a[0]
+second_str = a[1]
+
+first_num = int(first_str)
+second_num = int(second_str)
+
+res = first_num + second_num
+
+print(res)
+
+
+
+
+
