4d5573f6fb4bb46e135efae98ed3287b31b3feed
mehtavandit <65803900+mehtavandit@users.noreply.github.com>
2022-04-11 11:25:19 +0530

    Update challenge_1.py

--- day 048/challenge_1.py
+++ day 048/challenge_1.py
@@ -1,1 +1,22 @@
+from selenium import webdriver
+import time
 
+from selenium.webdriver.common.by import By
+
+chrome_driver_path = "C:\Development\chromedriver.exe"
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+driver.get("https://www.python.org/")
+
+times = driver.find_elements(By.CSS_SELECTOR, ".event-widget time")
+names = driver.find_elements(By.CSS_SELECTOR, ".event-widget li a")
+
+upcoming_events = {}
+
+for n in range(len(times)):
+    upcoming_events[n] = {
+        "time" : times[n].text,
+        "name" : names[n].text
+    }
+
+print(upcoming_events)
